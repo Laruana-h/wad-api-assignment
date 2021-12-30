35b88fa0ab5c8c5abf6d6c43eb0561e592515bd7
Sijie He <1274718084@qq.com>
2021-12-30 19:58:53 +0000

    get movies,genres,tvs,actors from TMDB

--- seedData/index.js
+++ seedData/index.js
@@ -4,11 +4,12 @@ import users from './users';
 import genres from './genres';
 import dotenv from 'dotenv';
 import movieModel from '../api/movies/movieModel';
-import movies from './movies.js';
 import actorsModel from '../api/actors/actorsModel';
 import actors from './actors.js';
 import tvModel from '../api/tvs/tvModel';
 import tvs from './tvs.js';
+
+const {getMovies} = require('../api/tmdb-api')
 
 dotenv.config();
 
@@ -37,9 +38,10 @@ async function loadGenres() {
 // deletes all movies documents in collection and inserts test data
 export async function loadMovies() {
   console.log('load seed data');
-  console.log(movies.length);
+  // console.log(movies.length);
   try {
     await movieModel.deleteMany();
+    const movies = await getMovies();
     await movieModel.collection.insertMany(movies);
     console.info(`${movies.length} Movies were successfully stored.`);
   } catch (err) {
